01eca99953b69e7f9a9a6e70a8f4103d5d3757ce
Sebastian Inman <sebastian.inman@highwayproducts.com>
2017-09-22 11:52:33 -0700

    fixed config select menu

--- init.sh
+++ init.sh
@@ -6,20 +6,29 @@ echo "Ubuntu 16.04 Web Server Configuration Shell Scripts"
 echo "Created by Sebastian Inman <sebastian@inman.design>"
 echo "==================================================="
 echo "                                                   "
-echo "                                                   "
 
 
-PS3='Please select which web server you would like to configure:'
-select SERVER in apache nginx cancel; do
-  case "$SERVER" in
-    "apache")
-      echo "install apache..." ;;
-    "nginx")
-      echo "install nginx..." ;;
-    "cancel")
-      break ;;
-  esac
-done
+Initialize() {
+  UpgradePackages
+  ConfigureServer
+}
+
+
+ConfigureServer() {
+  PS3="Please select which web server you would like to configure:"
+  select SERVER in apache nginx cancel; do
+    case "$SERVER" in
+      "apache")
+        source apache-setup.sh
+        ;;
+      "nginx")
+        source nginx-setup.sh
+        ;;
+      "cancel")
+        break ;;
+    esac
+  done
+}
 
 
 UpdatePackages() {
@@ -29,8 +38,11 @@ UpdatePackages() {
 
 
 UpgradePackages() {
-  echo "Please wait while the system packages are upgraded..."
   UpdatePackages
+  echo "Please wait while the system packages are upgraded..."
   sudo apt upgrade -y
   sudo apt autoremove -y
 }
+
+
+Initialize
