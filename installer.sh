1fd87d5e09663079769a82ad847297f276c5e8ac
Sebastian Inman <sebastian.inman@highwayproducts.com>
2017-09-29 09:59:09 -0700

    fixed color formatting

--- installer.sh
+++ installer.sh
@@ -81,7 +81,7 @@ PromptSettings() {
 
 
 AddSystemUser() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Creating new user '$USERNAME'..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Creating new user '$USERNAME'..."
   sudo adduser $USERNAME --gecos "$REAL_NAME,,," --disabled-password > $SCRIPT_FOLDER/installer.log 2>&1
   echo "$USERNAME:$USER_PASSWORD" | sudo chpasswd > $SCRIPT_FOLDER/installer.log 2>&1
   sudo usermod -aG sudo $USERNAME > $SCRIPT_FOLDER/installer.log 2>&1
@@ -91,13 +91,13 @@ AddSystemUser() {
 
 
 UpdatePackages() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Checking for package updates..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Checking for package updates..."
   sudo apt-get update > $SCRIPT_FOLDER/installer.log 2>&1
 }
 
 
 InstallUpdates() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing package updates..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing package updates..."
   sudo apt-get -y upgrade > $SCRIPT_FOLDER/installer.log 2>&1
 }
 
@@ -133,7 +133,7 @@ ConfigureSystem() {
 
 
 InstallDependencies() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing package dependencies..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing package dependencies..."
   # Install the UFW package
   sudo apt-get install -y ufw > $SCRIPT_FOLDER/installer.log 2>&1
   # Install the unzip package
@@ -150,7 +150,7 @@ InstallDependencies() {
 
 
 ConfigureFirewall() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring firewall..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring firewall..."
   # Allow SSH through firewall
   sudo ufw allow ssh > $SCRIPT_FOLDER/installer.log 2>&1
   # Allow HTTP through firewall
@@ -163,7 +163,7 @@ ConfigureFirewall() {
 
 
 InstallPHP() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing PHP with core modules..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing PHP with core modules..."
   # Download the most recent PHP repository
   sudo add-apt-repository -y ppa:ondrej/php > $SCRIPT_FOLDER/installer.log 2>&1
   # Check for package updates
@@ -186,7 +186,7 @@ ConfigurePHP() {
 
 
 InstallMySQL() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing MySQL..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing MySQL..."
   # Check for package updates
   UpdatePackages
   # Configure the MySQL username and password
@@ -200,7 +200,7 @@ InstallMySQL() {
 
 
 ConfigureMySQL() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring MySQL databases..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring MySQL databases..."
   # Update temp variables in the installer MySQL file
   sudo sed -i "s/%DATABASE%/$DATABASE/g" $SCRIPT_FOLDER/databases/installer.sql
   sudo sed -i "s/%USERNAME%/$USERNAME/g" $SCRIPT_FOLDER/databases/installer.sql
@@ -217,7 +217,7 @@ RestartPHPService() {
 
 
 InstallNginx() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing the Nginx server..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing the Nginx server..."
   # Download the most recent Nginx repository
   sudo add-apt-repository -y ppa:nginx/development > $SCRIPT_FOLDER/installer.log 2>&1
   # Check for package updates
@@ -230,7 +230,7 @@ InstallNginx() {
 
 
 ConfigureNginx() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the Nginx server to host $SITE_DOMAIN..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the Nginx server to host $SITE_DOMAIN..."
   # Enable the PHP script module in Nginx
   sudo echo 'fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;' >> /etc/nginx/fastcgi_params
   # Backup the original Nginx config file
@@ -251,7 +251,7 @@ RestartNginxService() {
 
 
 ConfigureWebServer() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the $SITE_DOMAIN server block..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the $SITE_DOMAIN server block..."
   # Create web server directories
   sudo mkdir -p /home/$USERNAME/$SITE_DOMAIN/backups
   sudo mkdir -p /home/$USERNAME/$SITE_DOMAIN/public
@@ -302,7 +302,7 @@ InstallSSLCertificate() {
 
 
 InstallWordPress() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Downloading and installing the latest WordPress build..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Downloading and installing the latest WordPress build..."
   # Download the latest version of WordPress
   curl -s -o /home/$USERNAME/wordpress.zip https://wordpress.org/latest.zip
   # Unzip the WordPress download
@@ -319,7 +319,7 @@ InstallWordPress() {
 
 
 ConfigureWordPress() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the WordPress installation..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Configuring the WordPress installation..."
   # Update temp variables in the wp-config file
   sudo sed -i "s/%DATABASE%/$DATABASE/g" $SCRIPT_FOLDER/wordpress/wp-config.php
   sudo sed -i "s/%USERNAME%/$USERNAME/g" $SCRIPT_FOLDER/wordpress/wp-config.php
@@ -332,7 +332,7 @@ ConfigureWordPress() {
 
 
 InsallWordPressPlugins() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Installing WordPress plugins..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Installing WordPress plugins..."
   # Delete any existing WordPress plugins
   sudo rm -r /home/$USERNAME/$SITE_DOMAIN/public/wp-content/plugins/*
   # Install default WordPress plugins
@@ -349,7 +349,7 @@ ConfigureCache() {
 
 
 RestartServices() {
-  echo "${CLR_YELLOW}[ * ]${CLR_RESET} Restarting system services..."
+  echo -e "${CLR_YELLOW}[ * ]${CLR_RESET} Restarting system services..."
   # Restart the Redis cache service
   sudo service redis-server restart
   # Restart the PHP service
@@ -375,9 +375,9 @@ StartInstaller() {
   RestartServices
 
   echo ""
-  echo "${CLR_GREEN}Installation finished in $(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds!"
-  echo "Your server password is: $USER_PASSWORD"
-  echo "Your MySQL password is: $MYSQL_PASSWORD"
+  echo -e "${CLR_GREEN}Installation finished in $(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds!"
+  echo -e "${CLR_RESET}Your server password is:${CLR_CYAN} $USER_PASSWORD"
+  echo -e "${CLR_RESET}Your MySQL password is:${CLR_CYAN} $MYSQL_PASSWORD"
   echo ""
 
 }
